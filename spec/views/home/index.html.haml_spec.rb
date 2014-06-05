85abf4987c93391221cf7d21e84c47dcf6771c60
ArturT <arturtrzop@gmail.com>
2014-06-06 01:42:05 +0200

    Remove spec/views

--- spec/views/home/index.html.haml_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'home/index.html.haml' do
-  it 'view has content Hello World', :railstom do
-    render
-    expect(rendered).to have_content('Hello World')
-  end
-end
