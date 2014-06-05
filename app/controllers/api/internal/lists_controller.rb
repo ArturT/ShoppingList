d90eb3f3cbbe5ef93509d8bc999ab1e61370f2de
ArturT <artur.trzop@lunarlogic.io>
2014-06-05 19:38:27 +0200

    update lists controller

--- app/controllers/api/internal/lists_controller.rb
+++ app/controllers/api/internal/lists_controller.rb
@@ -1,17 +1,31 @@
 class Api::Internal::ListsController < ApplicationController
   before_action :authenticate_user!
-  respond_to :json
 
   def index
-    respond_with current_user.lists
+    render json: current_user.lists
   end
 
   def create
+    list = current_user.lists.build(list_params)
+    if list.save
+      render json: list
+    else
+      render json: { errors: list.errors }
+    end
   end
 
   def update
   end
 
   def destroy
+    list = current_user.lists.find(params[:id])
+    list.destroy
+    render json: {}
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
