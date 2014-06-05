107ee6bf41899bc74a7fd285867537d76adbee1a
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-06-05 18:02:08 +0200

    added Item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605155657) do
+ActiveRecord::Schema.define(version: 20140605160124) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "resource_id",   null: false
@@ -55,6 +55,12 @@ ActiveRecord::Schema.define(version: 20140605155657) do
   end
 
   add_index "authentications", ["user_id"], name: "index_authentications_on_user_id", using: :btree
+
+  create_table "items", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "lists", force: true do |t|
     t.string   "name"
