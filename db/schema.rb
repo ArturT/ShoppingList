250eb11f9b9423e75f7ab952874ea6965f4a713f
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-06-05 18:33:33 +0200

    added user_id to list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605160446) do
+ActiveRecord::Schema.define(version: 20140605162955) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "resource_id",   null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20140605160446) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "newsletters", force: true do |t|
